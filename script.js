c4c91638dd5cc09b714dda9f932d2d7fbcb07671
Szymon <szymon.tomasz.szadkowski@gmail.com>
2020-08-22 18:15:05 +0200

    Debugged menu

--- script.js
+++ script.js
@@ -1,37 +1,26 @@
 // sidemenu
 
-const menu = document.querySelector(".nav-menu");
-const nav_container = document.querySelector(".nav-container");
-const bars_btn = document.querySelector(".bars");
-const close_btn = document.querySelector(".menu-exit");
+var menu = document.querySelector(".nav-menu");
+var nav_container = document.querySelector(".nav-container");
+var bars_btn = document.querySelector(".bars");
+var close_btn = document.querySelector(".menu-exit");
 
 bars_btn.addEventListener("click", () =>{
     menu.classList.toggle("toggle-menu");
-    if (nav_container.style.backgroundColor !== "rgba(245, 245, 245, 0.9)"){
-        nav_container.style.backgroundColor = "transparent";
-        nav_container.style.boxShadow = "none";
-    }
-    else if(nav_container.style.backgroundColor === "transparent"){
-        nav_container.style.backgroundColor = "rgba(245, 245, 245, 0.9)";
-        nav_container.style.boxShadow = "1px 1px 5px 1px rgba(128, 128, 128, 0.5)"; 
-    }
 });
-
-// close_btn.addEventListener("click", () =>{
-//     menu.classList.remove("toggle-menu");
-// });
 
 // dynamic date
 
-const date = document.getElementById("date");
+var date = document.getElementById("date");
 date.textContent = new Date().getFullYear();
 
 // fixed navbar
-const top_btn = document.querySelector(".top");
+var top_btn = document.querySelector(".top");
+var nav_height = nav_container.getBoundingClientRect().height;
 
 window.addEventListener("scroll", () =>{
-    const page_height = window.pageYOffset;
-    const nav_height = nav_container.getBoundingClientRect().height;
+    var page_height = window.pageYOffset;
+    var nav_height = nav_container.getBoundingClientRect().height;
     if (page_height > nav_height){
         nav_container.style.backgroundColor = "rgba(245, 245, 245, 0.9)";
         nav_container.style.boxShadow = "1px 1px 5px 1px rgba(128, 128, 128, 0.5)";
@@ -51,18 +40,19 @@ window.addEventListener("scroll", () =>{
 
 // scroll repair
 
-const scroll_links = document.querySelectorAll(".scroll_link");
+var scroll_links = document.querySelectorAll(".scroll-link");
 
-scroll_links.forEach(link => {
-    link.addEventListener("click", (e) =>{
-        console.log(position);
-        e.preventDefault();
-        const e_id = e.currentTarget.getAttribute("href").slice(1);
-        const element = document.getElementById(e_id);
-        let position = element.offsetTop;
+scroll_links.forEach((link) => {
+    link.addEventListener("click", (event) =>{
+        event.preventDefault();
+        var event_id = event.currentTarget.getAttribute("href").slice(1);
+        var element = document.getElementById(event_id);
+        let position = element.offsetTop - nav_height;
         window.scrollTo({
             left: 0,
-            top: position + 100,
+            top: position,
         });
+        // make manu disappear after selecting a section to go to
+        menu.classList.toggle("toggle-menu");
     });
 });
